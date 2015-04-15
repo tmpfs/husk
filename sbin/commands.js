ca2aea54c1042531a1b35fad06d8960756ee7002
cyberfunk <hello@cyberfunk.org>
2015-04-15 19:31:16 +0800

    Work on supporting pipe fd option.

--- sbin/commands.js
+++ sbin/commands.js
@@ -7,6 +7,10 @@ module.exports = {
     cmd: 'ebin/pwd',
     description: 'Execute a command'
   },
+  sleep: {
+    cmd: 'ebin/sleep',
+    description: 'Sleep for a bit'
+  },
   who: {
     cmd: 'who | ebin/who',
     description: 'Pipe stdin to various plugins to produce json'
