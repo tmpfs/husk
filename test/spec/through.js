c7e9b644bbd9c535f55a8b15443b8b59fd9dcfea
cyberfunk <hello@cyberfunk.org>
2015-04-21 23:19:58 +0800

    Assert on through plugin.

---
+++ test/spec/through.js
@@ -0,0 +1,21 @@
+var expect = require('chai').expect
+  , husk = require('../..');
+
+describe('husk:', function() {
+
+  it('should pass through function', function(done) {
+    var h = husk({})
+      .through(function(){this.field = 'value'})
+      .assert(function() {
+        return this.field === 'value';
+      })
+      .run(done);
+  });
+
+  it('should pass through with no function', function(done) {
+    var h = husk({})
+      .through()
+      .run(done);
+  });
+
+});
