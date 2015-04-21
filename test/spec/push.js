8507dfc71c119cfcd0fee231dd9b75406ac53baa
cyberfunk <hello@cyberfunk.org>
2015-04-21 23:24:03 +0800

    Assert on push plugin.

---
+++ test/spec/push.js
@@ -0,0 +1,39 @@
+var expect = require('chai').expect
+  , husk = require('../..');
+
+describe('husk:', function() {
+
+  it('should pass through with no function', function(done) {
+    var h = husk({})
+      .push()
+      .run(done);
+  });
+
+  it('should push with sync callback', function(done) {
+    var h = husk()
+      .push(function(){this.push({field: 'value'})})
+      .assert(function() {
+        return this.field === 'value';
+      })
+      .run(done);
+  });
+
+  it('should push with async callback', function(done) {
+    var h = husk()
+      .push(function(chunk, cb){this.push({field: 'value'}); cb();})
+      .assert(function() {
+        return this.field === 'value';
+      })
+      .run(done);
+  });
+
+  it('should push with async encoding callback', function(done) {
+    var h = husk()
+      .push(function(chunk, encoding, cb){this.push({field: 'value'}); cb();})
+      .assert(function() {
+        return this.field === 'value';
+      })
+      .run(done);
+  });
+
+});
--- test/spec/through.js
+++ test/spec/through.js
@@ -3,18 +3,18 @@ var expect = require('chai').expect
 
 describe('husk:', function() {
 
+  it('should pass through with no function', function(done) {
+    var h = husk({})
+      .through()
+      .run(done);
+  });
+
   it('should pass through function', function(done) {
     var h = husk({})
       .through(function(){this.field = 'value'})
       .assert(function() {
         return this.field === 'value';
       })
-      .run(done);
-  });
-
-  it('should pass through with no function', function(done) {
-    var h = husk({})
-      .through()
       .run(done);
   });
 
