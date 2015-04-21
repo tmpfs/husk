512052903b3e1c4c4af9696cf4cb103601645617
cyberfunk <hello@cyberfunk.org>
2015-04-21 19:41:03 +0800

    Assert on main class.

--- test/spec/husk.js
+++ test/spec/husk.js
@@ -5,6 +5,22 @@ describe('husk:', function() {
 
   it('should export function', function(done) {
     expect(husk).to.be.a('function');
+    var h = husk();
+    expect(h).to.be.instanceof(husk.Type);
+    // new less style
+    h = husk.Type(null, {end: false});
+    expect(h).to.be.instanceof(husk.Type);
+    expect(h.opts).to.be.an('object');
+    expect(h.pipeline).to.be.an('array');
+    expect(h.data).to.eql(null);
+    done();
+  });
+
+  it('should load plugins', function(done) {
+    var h = husk.exec();
+    expect(h).to.equal(husk);
+    h = husk.fs();
+    expect(h).to.equal(husk);
     done();
   });
 });
