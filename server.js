06bdd279bf87d2b95be498cd658bca660cd4b2fe
cyberfunk <hello@cyberfunk.org>
2015-04-26 11:18:11 +0800

    Draft http plugin, mock server.

---
+++ server.js
@@ -0,0 +1,26 @@
+var http = require('http')
+  , port = parseInt(process.env.TEST_PORT) || 3000
+  , server = http.createServer(request);
+
+/**
+ *  Simple echo server for test specs.
+ */
+function request(req, res) {
+  var msg = {headers: req.headers, method: req.method, url: req.url}
+    , body = JSON.stringify(msg, undefined, 2);
+  res.writeHead(200, {
+    'content-type': 'application/json',
+    'content-length': Buffer.byteLength(body)});
+  res.end(body);
+}
+
+function start(cb) {
+  server.listen(port, cb);
+  return server;
+}
+
+if(!module.parent) {
+  start();
+}
+
+module.exports = start;
